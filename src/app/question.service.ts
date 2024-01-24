caadbf980f10175ec6ab3038574be527c4b648d7
danielpolak2000 <daniel.polak@web.de>
2024-01-24 15:45:31 +0100

    update

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -9,6 +9,6 @@ export class QuestionService {
   constructor(private http: HttpClient) {}
 
   getQuestions(): Observable<any> {
-    return this.http.get('assets/questions.txt', { responseType: 'text' });
+    return this.http.get('assets/questions_small.txt', { responseType: 'text' });
   }
 }
